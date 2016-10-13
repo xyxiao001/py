17ef57c7b3ad620a9158d3da7802affe6d28b034
xy xiao <xy xiao>
2016-10-13 21:47:50 +0800

    study 4

---
+++ 4/number.py
@@ -0,0 +1,24 @@
+import math
+import random
+
+a = [1, 2, 3]
+print(a)
+
+print(1.5 * 4)
+
+print(2 ** 2)
+
+# 返回 2的 1万 次方的 长度
+print(len(str(2 ** 10000)))
+
+print(math.pi)
+
+#随机数 0 - 9  js Math.random() * 10
+print(random.random() * 10)
+
+# 从数组中取随机数
+# js var arr = [1, 2, 3, 4] arr[~~(Math.random() * 10)]
+print(random.choice([1, 2, 3, 4]))
+
+# math.sqrt 开平方
+print(math.sqrt(81))
