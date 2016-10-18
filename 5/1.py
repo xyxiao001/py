34cfabb9d5ac8bc7aeb6dc2377944f38ba7d3c84
xyxiao001 <463618622@qq.com>
2016-10-18 22:01:20 +0800

    study number

---
+++ 5/1.py
@@ -0,0 +1,26 @@
+# 数字类型
+# 复数
+a = complex(1, 2)
+print(a)
+# (1 + 2j)
+print(int(1.59))
+# 1
+
+b = 1 / 3
+print(b)
+# 0.3333333
+print('%4.2f' % b)
+# 0.33
+print('{0:4.2f}'.format(b))
+# 0.33
+
+x = 1
+y = 2
+z = 3
+print(x < y < z)
+# True
+
+print(1 / 3)
+# 0.333333
+print(1 // 3)
+# 0
