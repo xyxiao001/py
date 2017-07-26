d4b641c2370fce40c004dc6be35eefdda79d3525
xyxiao <463618622@qq.com>
2017-07-26 11:32:07 +0800

    add some

---
+++ 5/fractions.py
@@ -0,0 +1,10 @@
+from fractions import Fraction
+x = Fraction(1, 3)
+y = Fraction(4, 6)
+print(x)
+print(y)
+
+z = x + y
+print(z)
+
+print(0.1 + 0.1 + 0.1 - 0.3)
---
+++ 5/set.py
@@ -0,0 +1,30 @@
+x= set('abcde')
+y = set('bdxyz')
+print(x)
+print(y)
+print('e' in x)
+print(x - y)
+print(x & y)
+print(x ^ y)
+print(x > y, x < y)
+# intersection 是区两个集合相同的值
+z = x.intersection(y)
+print(z)
+z.add('SPAM')
+print(z)
+z.update(set(['X', 'Y']))
+print(z)
+z.remove('b')
+print(z)
+
+for item in set('abc'): print(item * 3)
+
+a = set('1234')
+print(a)
+b = set('23456')
+# union 会过滤掉重复的值
+c = a.union(b)
+print(c)
+
+d = {x ** 2 for x in [1, 2, 3, 4]}
+print(d)
